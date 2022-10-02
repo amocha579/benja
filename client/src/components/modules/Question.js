8aac8ec2287edfad6aa6870744da75e2e6120a34
Aileen Han <ahan@mit.edu>
2022-10-01 22:03:23 -0400

    Added Question component

---
+++ client/src/components/modules/Question.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+import "./Question.css";
+
+/**
+ * Story is a component that renders creator and content of a story
+ *
+ * Proptypes
+ * @param {string} question title
+ * @param {string[]} answers array of answer choices
+ */
+class Question extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    let answerString = [];
+    for(let i = 0; i < this.props.answers.length; i++) {
+        answerString.push(<p className="Question-option u-inline">{this.props.answers[i]}</p>);
+    }
+
+    return (
+      <div className="Question-box">
+        <h4 className="Question-title u-inline"> {this.props.title} </h4>
+        
+        {answerString}
+      </div>
+    );
+  }
+}
+
+export default Question;
